28475d21ed3a3a3a7dffc0b4ea3940aabdd3848b
Adam Sinning <asinning25@gmail.com>
2019-09-07 12:40:28 -0600

    react

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
