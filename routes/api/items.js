f4f7c8fab54b425ed03860cb32cd19f08654eb8b
Adam Sinning <asinning25@gmail.com>
2019-09-07 12:03:30 -0600

    api routes added

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,5 +13,25 @@ router.get('/', (req, res) => {
     .then(items => res.json(items))
 });
 
+//@route POST api/items
+//Create an Item
+//@access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save()
+    .then(item => res.json(item));
+});
+
+//@route Delete api/items/:id
+//Delete an Item
+//@access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: alse}));
+});
+
 
 module.exports = router;
